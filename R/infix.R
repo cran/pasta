ca8e7971ddb89e240ca65b1961962f6efa0afa93
Christoph Schürz <c.schuerz@gmail.com>
2016-12-12 17:16:31 +0000

    version 0.1.0

---
+++ R/infix.R
@@ -0,0 +1,69 @@
+#' Make pasta with Strings
+#'
+#' Pasta provides a set of easy to read infix function for the intuitive
+#' pasting of characters.\cr
+#' Currently the following functions are available for concatenation:
+#'
+#' @param a,b characters.
+#' @name pasta
+#' @examples
+#' 'macaroni' %_% 'sauce' # 'macaroni_sauce'
+#' 'ravioli' %-% 'recipe' # 'ravioli-recipe'
+#' 'spaghetti' %.% 'code' # 'spaghetti.code'
+#' 'fusilli' %//% 'farfalle' # 'fusilli/farfalle'
+#' 'pasta' %&% 'sciutta' # 'pastasciutta'
+#' 'noodle' %&&% 'soup' # 'noodle soup'
+#' 'carbonara' %|% 'ragù' # 'carbonara|ragù'
+#' 'spaghetti' %&&% 'al' %-% 'dente' # 'spaghetti al-dente'
+NULL
+
+#' Concatenate with an underscore
+#'
+#' \%_\% pastes two strings by "_".
+#' @rdname pasta
+#' @export
+"%_%" <- function(a, b) paste(a, b, sep = "_")
+
+#' Concatenate with a hyphen
+#'
+#' \%-\% pastes two strings by "-".
+#' @rdname pasta
+#' @export
+'%-%' <- function(a, b) paste(a, b, sep = "-")
+
+#' Concatenate with a dot
+#'
+#' \%.\% pastes two strings by ".".
+#' @rdname pasta
+#' @export
+'%.%' <- function(a, b) paste(a, b, sep = ".")
+
+#' Paste slash function
+#'
+#' \%//\% pastes two strings by "/".
+#' @rdname pasta
+#' @export
+'%//%' <- function(a, b) paste(a, b, sep = "/")
+
+#' Concatenate without separator
+#'
+#' \%&\% pastes two strings by "".
+#' @rdname pasta
+#' @export
+'%&%' <- function(a, b) paste0(a, b)
+
+#' Concatenate with space
+#'
+#' \%&&\% pastes two strings by " ".
+#' @rdname pasta
+#' @export
+'%&&%' <- function(a, b) paste(a, b, sep = " ")
+
+#' Concatenate with or
+#'
+#' \%|\% pastes two strings with a "|".
+#' @rdname pasta
+#' @export
+'%|%' <- function(a, b) paste(a, b, sep = "|")
+
+
